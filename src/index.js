4273993aa63e3a2a98c1f13e0b1f9e99f645b760
martincamer <martincamer011@gmail.com>
2024-03-23 17:05:31 -0300

    ASD

--- src/index.js
+++ src/index.js
@@ -9,15 +9,14 @@ import { Server } from "socket.io";
 // Crea el servidor HTTP utilizando Express
 const httpServer = createServer(app);
 
+// app.use(express.static(resolve("frontend/dist")));
+
 // Crea el servidor de Socket.io y adjúntalo al servidor HTTP
 const io = new Server(httpServer, {
   cors: {
     origin: ORIGIN,
     credentials: true,
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Access-Control-Allow-Origin"],
   },
-  maxHttpBufferSize: 1e8,
 });
 
 // Maneja eventos de Socket.io
@@ -27,9 +26,6 @@ io.on("connection", (socket) => {
   // Maneja el evento "nueva-salida"
   socket.on("nueva-salida", (datosSalida) => {
     console.log("Nueva salida:", datosSalida);
-    // Aquí puedes realizar acciones con los datos de la nueva salida, como almacenarlos en la base de datos
-    // o emitir el evento a otros clientes si es necesario
-    // Por ejemplo, puedes enviar un mensaje a todos los clientes conectados para informarles sobre la nueva salida
     io.emit("nueva-salida", datosSalida); // Esto emitirá el evento "nueva-salida" a todos los clientes conectados
   });
 
@@ -41,9 +37,7 @@ io.on("connection", (socket) => {
   // Maneja el evento "eliminar-salida"
   socket.on("eliminar-salida", (datosSalida) => {
     console.log("Eliminar salida:", datosSalida);
-    // Aquí puedes realizar acciones con los datos de la salida eliminada, como eliminarla de la base de datos
-    // o emitir el evento a otros clientes si es necesario
-    // Por ejemplo, puedes enviar un mensaje a todos los clientes conectados para informarles sobre la salida eliminada
+
     io.emit("eliminar-salida", datosSalida); // Esto emitirá el evento "eliminar-salida" a todos los clientes conectados
   });
 
@@ -53,25 +47,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on("actualizar-salidas", (nuevasSalidas) => {
-    // Procesar las nuevas salidas recibidas desde el cliente
     console.log("Nuevas salidas recibidas:", nuevasSalidas);
-    // Actualizar el estado, almacenar en la base de datos, etc.
   });
 
   socket.on("actualizar-choferes", (nuevosChoferes) => {
-    // Procesar los nuevos choferes recibidos desde el cliente
     console.log("Nuevos choferes recibidos:", nuevosChoferes);
-    // Actualizar el estado, almacenar en la base de datos, etc.
   });
-
-  // socket.on("disconnect", () => {
-  //   console.log("User disconnected");
-  // });
-
-  // Agrega más controladores de eventos de Socket.io aquí
 });
 
-// Escucha el servidor HTTP en el puerto especificado en la configuración
 httpServer.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
